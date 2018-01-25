b0d21de5988219445c8c03f231fc1880ac64243a
Paul Jarleton <sarlam@minet.net>
2018-01-25 16:24:59 +0100

    change direction to an enumeration case class

--- src/main/scala/me/jarleton/smacks/model/Adventurer.scala
+++ src/main/scala/me/jarleton/smacks/model/Adventurer.scala
@@ -1,16 +1,27 @@
 
 package me.jarleton.smacks.model
 
+object Directions {
+  sealed trait EnumVal
+  case object South extends EnumVal
+  case object Est extends EnumVal
+  case object North extends EnumVal
+  case object West extends EnumVal
+}
+
 case class Adventurer(
                        name: String,
                        var x: Int,
                        var y: Int,
                        path: Array[Char],
-                       var direction: Char,
+                       var direction: Directions.EnumVal,
                        var treasures: Int = 0
                      ) {
   def toRight = {
-
+    this.direction match {
+      case Directions.North => this.direction = Directions.Est
+      case Directions.Est => this.direction = Directions.South
+    }
   }
 
   def toLeft = {
--- src/test/scala/me/jarleton/smacks/model/AdventurerSpec.scala
+++ src/test/scala/me/jarleton/smacks/model/AdventurerSpec.scala
@@ -8,51 +8,51 @@ class AdventurerSpec extends FlatSpec with Matchers {
   final val NAME = "maria"
 
   "An Adventurer turning right" should "turn from South to West" in {
-    val a = Adventurer(NAME, 1, 1, "".toCharArray, 'S')
+    val a = Adventurer(NAME, 1, 1, "".toCharArray, Directions.South)
     a.toRight
-    assert(a.direction == 'O')
+    assert(a.direction == Directions.West)
   }
 
   it should "turn from West to North" in {
-    val a = Adventurer(NAME, 1, 1, "".toCharArray, 'O')
+    val a = Adventurer(NAME, 1, 1, "".toCharArray, Directions.West)
     a.toRight
-    assert(a.direction == 'N')
+    assert(a.direction == Directions.North)
   }
 
   it should "turn from North to Est" in {
-    val a = Adventurer(NAME, 1, 1, "".toCharArray, 'N')
+    val a = Adventurer(NAME, 1, 1, "".toCharArray, Directions.North)
     a.toRight
-    assert(a.direction == 'E')
+    assert(a.direction == Directions.Est)
   }
 
   it should "turn from Est to South" in {
-    val a = Adventurer(NAME, 1, 1, "".toCharArray, 'E')
+    val a = Adventurer(NAME, 1, 1, "".toCharArray, Directions.Est)
     a.toRight
-    assert(a.direction == 'S')
+    assert(a.direction == Directions.South)
   }
 
   "An Adventurer turning left" should "turn from South to Est" in {
-    val a = Adventurer(NAME, 1, 1, "".toCharArray, 'S')
+    val a = Adventurer(NAME, 1, 1, "".toCharArray, Directions.South)
     a.toLeft
-    assert(a.direction == 'E')
+    assert(a.direction == Directions.Est)
   }
 
   it should "turn from Est to North" in {
-    val a = Adventurer(NAME, 1, 1, "".toCharArray, 'E')
+    val a = Adventurer(NAME, 1, 1, "".toCharArray, Directions.Est)
     a.toLeft
-    assert(a.direction == 'N')
+    assert(a.direction == Directions.North)
   }
 
   it should "turn from North to West" in {
-    val a = Adventurer(NAME, 1, 1, "".toCharArray, 'N')
+    val a = Adventurer(NAME, 1, 1, "".toCharArray, Directions.North)
     a.toRight
-    assert(a.direction == 'O')
+    assert(a.direction == Directions.West)
   }
 
 
   it should "turn from West to South" in {
-    val a = Adventurer(NAME, 1, 1, "".toCharArray, 'O')
+    val a = Adventurer(NAME, 1, 1, "".toCharArray, Directions.West)
     a.toRight
-    assert(a.direction == 'S')
+    assert(a.direction == Directions.South)
   }
 }
